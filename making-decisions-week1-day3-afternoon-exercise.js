a23165c7ba0768ed0566d7c89aa034f5b3cd999c
Tori Daughton <toriddaughton@gmail.com>
2022-03-23 21:21:53 -0600

    all problems solved

--- making-decisions-week1-day3-afternoon-exercise.js
+++ making-decisions-week1-day3-afternoon-exercise.js
@@ -132,10 +132,10 @@ let passingScore = 7;
 
 //CODE HERE
 
-while(score < passingScore) {
-  console.log(score,"your score is not high enough")
-  score++
-} 
+// while(score < passingScore) {
+//   console.log(score,"your score is not high enough")
+//   score++
+// } 
 
 ////////// INTERMEDIATE PROBLEMS //////////
 
@@ -148,10 +148,23 @@ while(score < passingScore) {
 
 //CODE HERE
 
+// let changeMyMind = true
+
+// if (changeMyMind = true) {
+//   console.log(changeMyMind = false)
+// }
+
+// else if(changeMyMind = false){
+//   console.log(changeMyMind = true)
+// }
+
+
 ////////// PROBLEM 10 //////////
 // Using the "not" operator (!), change the current value of changeMyMind to true (it should currently be set to false due to the if-else statement in Problem 9). After you change the value of changeMyMind, console.log it's new value (it should now read true).
 
 //CODE HERE
+
+  // console.log(!changeMyMind)
 
 ////////// ADVANCED PROBLEMS //////////
 
@@ -161,3 +174,12 @@ let z = 5;
 // Create a while loop that continues to run while z is greater than 0. Within the while loop, log a countdown from the value of z to 1. Once you have logged the countdown, make sure you decrement z by 1. If your code is running properly, you should see: 5,4,3,2,1,4,3,2,1,3,2,1,2,1,1.
 
 //CODE HERE
+
+
+while(z > 0){
+  for(let i = z; i > 0; i--){
+    console.log(i)
+  }
+z--;
+}
+
